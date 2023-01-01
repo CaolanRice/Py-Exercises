d8e6c472d0d77f3aa9352437888134e716b00b5c
CaolanRice <c_f_rice@hotmail.co.uk>
2023-01-01 21:43:08 +0000

    First commit, four simple coding solutions to start the repo off!

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+#Function that  determine if numbers are divisible by 3, 5, or both 3 and 5. 
+
+def fizzBuzz(upTo):
+    for i in range(1, upTo +1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz', end=" ")
+        elif i % 3 == 0:
+            print('Fizz', end=" ")
+        elif i % 5 == 0:
+            print('Buzz', end=" ")
+        else:
+            print(i, end=" ")
+
+fizzBuzz(5)
