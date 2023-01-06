5e1537a775801761c72cd1e3d9754ce577fd456c
CaolanRice <c_f_rice@hotmail.co.uk>
2023-01-06 18:47:25 +0000

    fixing indent error in find and replace

--- find_and_replace.py
+++ find_and_replace.py
@@ -1,17 +1,14 @@
-
-
 def findAndReplace(text, oldText, newText):
     replacedText = ""
-
     i = 0
 
     while i < len(text):
         if text[i:i + len(oldText)] == oldText:
             replacedText += newText
             i += len(oldText)
-    else:
-        replacedText += text[i]
-        i += 1
+        else:
+            replacedText += text[i]
+            i += 1
     return replacedText
 
 
